6b23c79912a33aec2aa381a08b04bad28a4a55f2
Freya Kailing <fkailing@ssdl137-21.public.oberlin.edu>
2023-12-18 17:41:12 -0500

    block moves progress bar appropriately

--- game/src/rhythm.rs
+++ game/src/rhythm.rs
@@ -19,9 +19,13 @@ use fyrox::{
         node::{Node},
         Scene, SceneLoader,
     },
-    script::{ScriptContext, ScriptTrait},
+    script::{ScriptContext, ScriptTrait, ScriptMessagePayload, ScriptMessageContext},
 };
 use std::path::Path;
+
+const GOOD_DISTANCE: f32 = 0.2;
+const GREAT_DISTANCE: f32 = 0.1;
+const TOUCH_DISTANCE: f32 = 1.0;
 
 #[derive(Visit, Reflect, Debug, Clone, Default)]
 pub struct RhythmBlock{
@@ -33,11 +37,16 @@ pub struct RhythmBlock{
     clicked: bool,
 }
 
-const GOOD_DISTANCE: f32 = 0.2;
-const GREAT_DISTANCE: f32 = 0.1;
-const TOUCH_DISTANCE: f32 = 1.0;
+enum Message {
+    GreatClick,
+    GoodClick,
+    OkClick,
+    BadClick,
+    Win,
+    Loss,
+}
 
-impl_component_provider!(RhythmBlock,);
+impl_component_provider!(RhythmBlock);
 
 impl TypeUuidProvider for RhythmBlock {
     // Returns unique script id for serialization needs.
@@ -61,9 +70,11 @@ impl ScriptTrait for RhythmBlock {
     // Called whenever there is an event from OS (mouse click, keypress, etc.)
     fn on_os_event(&mut self, event: &Event<()>, context: &mut ScriptContext) {
         if let Event::WindowEvent { event, .. } = event {
-            if let &WindowEvent::MouseInput {button, state, .. } = event {
-                if button == MouseButton::Left {
-                    self.clicked = state == ElementState::Pressed;
+            if let WindowEvent::KeyboardInput { event, .. } = event {
+                let is_pressed = event.state == ElementState::Pressed;
+                match event.physical_key {
+                    fyrox::keyboard::PhysicalKey::Code(KeyCode::ArrowUp) => self.clicked = event.state == ElementState::Pressed,
+                    _ => (),
                 }
             }
         }
@@ -114,12 +125,107 @@ impl ScriptTrait for RhythmBlock {
 
                 if self.clicked == true {
                     rectangle.set_visibility(false);
+
+                    if self.great {
+                        context.message_sender.send_global(Message::GreatClick);
+                    } else if self.good {
+                        context.message_sender.send_global(Message::GoodClick);
+                    } else if self.touching {
+                        context.message_sender.send_global(Message::OkClick);
+                    } else {
+                        context.message_sender.send_global(Message::BadClick);
+                    }
                 }
             }
         }
     }
 
     // Returns unique script ID for serialization needs.
+    fn id(&self) -> Uuid {
+        Self::type_uuid()
+    }
+}
+
+#[derive(Visit, Reflect, Default, Debug, Clone)]
+pub struct ProgressBar {
+    progress: f32,
+    new_progress: f32,
+    move_amount: f32,
+    reference_block: Handle<Node>,
+}
+
+impl_component_provider!(ProgressBar);
+
+impl TypeUuidProvider for ProgressBar {
+    fn type_uuid() -> Uuid {
+        uuid!("ba1ccd7e-9def-11ee-8c90-0242ac120002")
+    }
+}
+
+impl ScriptTrait for ProgressBar {
+    fn on_init(&mut self, context: &mut ScriptContext) {
+        // Put initialization logic here.
+        self.progress = 5.0;
+        self.new_progress = 0.0;
+        self.move_amount = 0.05;
+    }
+    
+    fn on_start(&mut self, context: &mut ScriptContext) {
+        // Put start logic - it is called when every other script is already initialized.
+        context.message_dispatcher.subscribe_to::<Message>(context.handle);
+    }
+
+    fn on_os_event(&mut self, event: &Event<()>, context: &mut ScriptContext) {
+        // Respond to OS events here.
+    }
+
+    fn on_update(&mut self, context: &mut ScriptContext) {
+        if let Some(ref_block) = context.scene.graph.try_get(self.reference_block){
+            //gets position of reference block
+            let ref_position = ref_block.local_transform().position().clone();
+            
+            //accesses its own rectangle node
+            if let Some(rectangle) = context.scene.graph[context.handle].cast_mut::<Rectangle>() {
+                //updates if it should go up or down
+                if self.new_progress != 0.0 {
+                    //moves progress bar
+                    let transform = rectangle.local_transform_mut();
+                    let y_offset = self.new_progress*self.move_amount;
+                    let offset_down = Vector3::new(0.0, y_offset, 0.0);
+                    transform.offset(offset_down);
+
+                    //changes progress
+                    self.progress += self.new_progress;
+                    self.new_progress = 0.0;
+
+                    //checks if win or loss
+                    if rectangle.local_transform().position().y >= ref_position.y {
+                        context.message_sender.send_global(Message::Win);
+                    } else if self.progress <= 0.0 {
+                        context.message_sender.send_global(Message::Loss);
+                    }
+                }
+            }
+        }
+    }
+
+    fn on_message(&mut self, message: &mut dyn ScriptMessagePayload, ctx: &mut ScriptMessageContext,
+    ) {
+        // React to clicks.
+        if let Some(Message::GreatClick) = message.downcast_ref::<Message>() {
+            self.new_progress = 4.0;
+        }
+        if let Some(Message::GoodClick) = message.downcast_ref::<Message>() {
+            self.new_progress = 2.0;
+        }
+        if let Some(Message::OkClick) = message.downcast_ref::<Message>() {
+            self.new_progress = 1.0;
+        }
+        if let Some(Message::BadClick) = message.downcast_ref::<Message>() {
+            self.new_progress = -1.0;
+        }
+    }
+
     fn id(&self) -> Uuid {
         Self::type_uuid()
     }
